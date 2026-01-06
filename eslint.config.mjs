9ca526e522554f3e4b6aa93bb19a775ea1e531ca
Jaden Scali <jadenscali132@gmail.com>
2026-01-06 07:58:43 -0500

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,15 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
