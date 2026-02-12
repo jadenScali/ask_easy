d4f1705fed0ece6ba0b7f55a3cf05624e778984f
Jaden Scali <jadenscali132@gmail.com>
2026-02-11 21:19:21 -0500

    feat: text area matching stone pallete and auto resize

--- src/app/components/room/classChat/ChatInput.tsx
+++ src/app/components/room/classChat/ChatInput.tsx
@@ -8,10 +8,7 @@ export default function ChatInput() {
     <div className="border-t bg-stone-50 p-4 relative z-20">
       <div className="max-w-4xl mx-auto">
         <div className="flex gap-4">
-          <Textarea
-            placeholder="Add to the discussion..."
-            className="flex-1 min-w-0 min-h-[60px]"
-          />
+          <Textarea placeholder="Ask a question..." />
           <Button className="h-10 shrink-0 bg-stone-900 hover:bg-stone-700">Post</Button>
         </div>
       </div>
