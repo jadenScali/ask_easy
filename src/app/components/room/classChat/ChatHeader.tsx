a61d4d32ca198dd4aff69600f5edb5df2650fa47
Jaden Scali <jadenscali132@gmail.com>
2026-02-12 01:15:21 -0500

    fix: slides closed by default

--- src/app/components/room/classChat/ChatHeader.tsx
+++ src/app/components/room/classChat/ChatHeader.tsx
@@ -8,15 +8,15 @@ import { SlideUpdateContext } from "../index";
 
 function SlideToggle() {
   // toggles slides on and off
-  const { isHidden, rerender } = useContext(SlideUpdateContext);
+  const { isSlidesVisible, rerender } = useContext(SlideUpdateContext);
   const isMDsize = useMediaQuery("(min-width: 1024px)");
   if (!isMDsize) {
     return (
       <button
-        className="w-10 h-10 flex items-center justify-center text-stone-400 hover:text-stone-700 hover:bg-stone-200 rounded-md transition-colors"
+        className="w-10 h-10 flex items-center justify-center text-stone-900/50 hover:text-stone-900 hover:bg-stone-200 rounded-md transition-colors"
         onClick={() => rerender()}
       >
-        {isHidden ? (
+        {isSlidesVisible ? (
           <PanelRightClose className="w-6 h-6 rotate-270" />
         ) : (
           <PanelRightClose className="w-6 h-6 rotate-90" />
@@ -26,10 +26,10 @@ function SlideToggle() {
   } else {
     return (
       <button
-        className="w-10 h-10 flex items-center justify-center text-stone-400 hover:text-stone-700 hover:bg-stone-200 rounded-md transition-colors"
+        className="w-10 h-10 flex items-center justify-center text-stone-400 hover:text-stone-900 hover:bg-stone-200 rounded-md transition-colors"
         onClick={() => rerender()}
       >
-        {isHidden ? (
+        {isSlidesVisible ? (
           <PanelRightClose className="w-6 h-6 rotate-180" />
         ) : (
           <PanelRightClose className="w-6 h-6 " />
@@ -42,7 +42,7 @@ function SlideToggle() {
 export default function ChatHeader() {
   return (
     <div className="flex flex-col border-b bg-background sticky bg-stone-50 top-0 z-10">
-      <header className="px-4 py-2 flex items-center justify-between">
+      <header className="pl-2 pr-4 py-2 flex items-center justify-between">
         <div className="flex items-center justify-left gap-2 ">
           <SlideToggle />
           <h1 className="text-xl font-bold">CSC209</h1>
